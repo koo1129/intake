e86f87ad3b0b0ac976865389a38046f029782c68
koo1129 <koo.1192icloud.com>
2020-03-25 17:23:58 +0900

    データ保存

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -6,7 +6,17 @@ class ClientsController < ApplicationController
     end
   
     def create
-        binding.pry
+        @client = Client.new(client_params)
+        if @client.save
+          redirect_to root_path, notice: 'グループを作成しました'
+        else
+          render :new
+        end
+    end
+
+    private
+    def client_params
+        params.require(:client).permit(:name, :sex, :job, :address, :from, :family, :character, :complaint, :history, :other, :intake, :phot, :email, :phone, user_ids: [])
     end
   
 end
